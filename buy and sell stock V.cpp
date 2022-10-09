386266593281aa3cfb7cdce3d0e4610b32a13906
amantayal2909 <76427320+amantayal2909@users.noreply.github.com>
2022-10-09 18:41:53 +0530

    fifth.cpp

---
+++ buy and sell stock V.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& p) {
+        int n=p.size();
+        vector<vector<int>> dp(n+2,vector<int>(2,0));
+        
+        for(int index=n-1;index>=0;index--)
+        {
+          dp[index][1]=max( (-p[index]+ dp[index+1][0])
+                          , dp[index+1][1]);
+          dp[index][0]=max( (p[index]+ dp[index+2][1])
+                          , dp[index+1][0]);  
+        }
+        return dp[0][1];
+    }
+};
